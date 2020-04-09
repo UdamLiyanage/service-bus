763caf5e17ee2e9cf07d686e7c21639584c76476
UdamLiyanage <udam1998@gmail.com>
2020-04-09 23:20:13 +0530

    Single node connection logic

--- nats_connect.go
+++ nats_connect.go
@@ -1,3 +1,13 @@
 package service_bus
 
-import ()
+import (
+	"github.com/nats-io/nats.go"
+)
+
+func SingleNodeConnect(url string) (*nats.Conn, error) {
+	nc, err := nats.Connect(url)
+	if err != nil {
+		return nil, err
+	}
+	return nc, nil
+}
