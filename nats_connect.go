2fefdb2cb3a3a6031bc969d6a654eb5d69ca95c4
UdamLiyanage <udam1998@gmail.com>
2020-04-09 23:20:39 +0530

    Cluster connect logic

--- nats_connect.go
+++ nats_connect.go
@@ -2,10 +2,19 @@ package service_bus
 
 import (
 	"github.com/nats-io/nats.go"
+	"strings"
 )
 
 func SingleNodeConnect(url string) (*nats.Conn, error) {
 	nc, err := nats.Connect(url)
+	if err != nil {
+		return nil, err
+	}
+	return nc, nil
+}
+
+func ClusterConnect(urls []string) (*nats.Conn, error) {
+	nc, err := nats.Connect(strings.Join(urls, ","))
 	if err != nil {
 		return nil, err
 	}
