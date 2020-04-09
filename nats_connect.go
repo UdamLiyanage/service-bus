d5e2a8b6e83a46a4f5657ed1724d14a97cf064df
UdamLiyanage <udam1998@gmail.com>
2020-04-09 23:52:23 +0530

    NATS Connection Options Set

--- nats_connect.go
+++ nats_connect.go
@@ -3,18 +3,35 @@ package service_bus
 import (
 	"github.com/nats-io/nats.go"
 	"strings"
+	"time"
 )
 
-func SingleNodeConnect(url string) (*nats.Conn, error) {
-	nc, err := nats.Connect(url)
+func SingleNodeConnect(url, name string) (*nats.Conn, error) {
+	nc, err := nats.Connect(
+		url,
+		nats.Name(name),
+		nats.Timeout(10*time.Second),
+		nats.PingInterval(20*time.Second),
+		nats.MaxPingsOutstanding(5),
+		nats.MaxReconnects(10),
+		nats.ReconnectWait(10*time.Second),
+		nats.ReconnectBufSize(5*1024*1024))
 	if err != nil {
 		return nil, err
 	}
 	return nc, nil
 }
 
-func ClusterConnect(urls []string) (*nats.Conn, error) {
-	nc, err := nats.Connect(strings.Join(urls, ","))
+func ClusterConnect(urls []string, name string) (*nats.Conn, error) {
+	nc, err := nats.Connect(
+		strings.Join(urls, ","),
+		nats.Name(name),
+		nats.Timeout(10*time.Second),
+		nats.PingInterval(20*time.Second),
+		nats.MaxPingsOutstanding(5),
+		nats.MaxReconnects(10),
+		nats.ReconnectWait(10*time.Second),
+		nats.ReconnectBufSize(5*1024*1024))
 	if err != nil {
 		return nil, err
 	}
