9301f68a46c89187bd0cbd14faadafae4a079e6b
UdamLiyanage <udam1998@gmail.com>
2020-04-10 08:40:02 +0530

    NATS Cluster Connect Test

--- nats_connect_test.go
+++ nats_connect_test.go
@@ -1,9 +1,25 @@
 package service_bus
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSingleNodeConnect(t *testing.T) {
 	nc, err := SingleNodeConnect("localhost:4222", "test-conn")
+	if err != nil {
+		t.Log("Error Occurred at SingleNodeConnect Function")
+		t.Error(err)
+	}
+	if !nc.IsConnected() {
+		t.Error("Not connected to server. SingleNodeConnect Function")
+	}
+}
+
+func TestClusterConnect(t *testing.T) {
+	urls := make([]string, 2)
+	urls = append(urls, "localhost:4222")
+	urls = append(urls, "localhost:5222")
+	nc, err := ClusterConnect(urls, "test-conn")
 	if err != nil {
 		t.Log("Error Occurred at SingleNodeConnect Function")
 		t.Error(err)
